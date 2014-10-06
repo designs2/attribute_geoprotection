3e81695cbef54384b54b143338be3670cd9ca112
Stefan Heimes <stefan_heimes@hotmail.com>
2014-10-06 16:40:10 +0200

    Add a missing language files for filter.

---
+++ src/system/modules/metamodelsattribute_geoprotection/languages/en/tl_metamodel_filtersetting.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package     MetaModels
+ * @subpackage  AttributeGeoProtection
+ * @author      Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author      David Maack <david.maack@arcor.de>
+ * @copyright   The MetaModels team.
+ * @license     LGPL.
+ * @filesource
+ */
+
+/**
+ * filter types
+ */
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['geoprotection'] = 'GeoProtection';
