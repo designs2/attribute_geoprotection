74dded36c43e2aea5c8cc1c9b7ddc72c426f877e
Stefan Heimes <stefan_heimes@hotmail.com>
2014-10-08 15:44:46 +0200

    Shorter the functions to get the list with all countries. Add some cache functions.

--- src/system/modules/metamodelsattribute_geoprotection/MetaModels/Attribute/Geoprotection/Helper.php
+++ src/system/modules/metamodelsattribute_geoprotection/MetaModels/Attribute/Geoprotection/Helper.php
@@ -28,6 +28,66 @@ namespace MetaModels\Attribute\Geoprotection;
 class Helper
 {
 	/**
+	 * A list with all countries information.
+	 *
+	 * @var array|null
+	 */
+	static protected $arrCountries;
+
+	/**
+	 * A list with the full list of the countries.
+	 *
+	 * @var array|null
+	 */
+	static protected $arrFullList;
+
+	/**
+	 * Build up the list with all countries information and cache this information.
+	 * Return all data from the cache.
+	 *
+	 * @return array|null
+	 */
+	static public function  getCountriesList()
+	{
+		if (empty(self::$arrCountries))
+		{
+			$countries = array();
+			$arrTmp    = array();
+
+			// Load the language files.
+			\System::loadLanguageFile('countries');
+			\System::loadLanguageFile('continents');
+
+			// Include all files with name.
+			include(TL_ROOT . '/system/config/countries.php');
+			include(TL_ROOT . '/system/config/countriesByContinent.php');
+
+			/** @var $countriesByContinent array */
+			foreach ($countriesByContinent as $strConKey => $arrCountries)
+			{
+				// Add the main value.
+				$strParentName = strlen($GLOBALS['TL_LANG']['CONTINENT'][$strConKey]) ? utf8_romanize($GLOBALS['TL_LANG']['CONTINENT'][$strConKey]) : $strConKey;
+
+				// Add all countries.
+				foreach (array_keys($arrCountries) as $key)
+				{
+					$arrTmp[$key] = array
+					(
+						'name'         => strlen($GLOBALS['TL_LANG']['CNT'][$key]) ? utf8_romanize($GLOBALS['TL_LANG']['CNT'][$key]) : $countries[$key],
+						'parent-name'  => $strParentName,
+						'parent-short' => $strConKey
+					);
+				}
+			}
+
+			self::$arrCountries = $arrTmp;
+		}
+
+		return self::$arrCountries;
+	}
+
+
+	/**
 	 * Get a list with all countries.
 	 *
 	 * @param array $arrValues a list with preset values.
@@ -36,51 +96,53 @@ class Helper
 	 */
 	public static function getCountriesByContinent($arrValues = array())
 	{
-		// Init all vars.
-		$return    = array();
-		$countries = array();
-		$arrAux    = array();
-		$arrTmp    = array();
+		$arrCountries = self::getCountriesList();
+		$arrReturn    = array();
 
-		// Load the language files.
-		\System::loadLanguageFile('countries');
-		\System::loadLanguageFile('continents');
-
-		// Include all files with name.
-		include(TL_ROOT . '/system/config/countries.php');
-		include(TL_ROOT . '/system/config/countriesByContinent.php');
-
-		/** @var $countriesByContinent array */
-		foreach ($countriesByContinent as $strConKey => $arrCountries)
+		if (empty($arrValues))
 		{
-			$strConKeyTranslated = strlen($GLOBALS['TL_LANG']['CONTINENT'][$strConKey]) ? utf8_romanize($GLOBALS['TL_LANG']['CONTINENT'][$strConKey]) : $strConKey;
-			$arrAux[$strConKey]  = $strConKeyTranslated;
-			foreach ($arrCountries as $key => $strCounntry)
+			if(empty(self::$arrFullList))
 			{
-				if (!is_array($arrValues) || in_array($key, $arrValues))
+				foreach ($arrCountries as $strShort => $arrInformation)
 				{
-					$arrTmp[$strConKeyTranslated][$key] = strlen($GLOBALS['TL_LANG']['CNT'][$key]) ? utf8_romanize($GLOBALS['TL_LANG']['CNT'][$key]) : $countries[$key];
+					$strName       = $arrInformation['name'];
+					$strParentName = $arrInformation['parent-name'];
+
+					$arrReturn[$strParentName][$strShort] = $strName;
 				}
 			}
-		}
-
-		ksort($arrTmp);
-
-		foreach ($arrTmp as $strConKey => $arrCountries)
-		{
-			asort($arrCountries);
-			//get original continent key
-			$strOrgKey           = array_search($strConKey, $arrAux);
-			$strConKeyTranslated = strlen($GLOBALS['TL_LANG']['CONTINENT'][$strOrgKey]) ? ($GLOBALS['TL_LANG']['CONTINENT'][$strOrgKey]) : $strConKey;
-			foreach ($arrCountries as $strKey => $strCountry)
+			else
 			{
-				$return[$strConKeyTranslated][$strKey] = strlen($GLOBALS['TL_LANG']['CNT'][$strKey]) ? $GLOBALS['TL_LANG']['CNT'][$strKey] : $countries[$strKey];
+				$arrReturn = self::$arrFullList;
+			}
+		}
+		else
+		{
+			foreach ($arrValues as $strShort)
+			{
+				$arrInformation = $arrCountries[$strShort];
+				$strName        = $arrInformation['name'];
+				$strParentName  = $arrInformation['parent-name'];
+
+				$arrReturn[$strParentName][$strShort] = $strName;
 			}
 		}
 
-		$return[$GLOBALS['TL_LANG']['CONTINENT']['other']]['xx'] = strlen($GLOBALS['TL_LANG']['CNT']['xx']) ? $GLOBALS['TL_LANG']['CNT']['xx'] : 'No Country';
+		// Add the other entry.
+		if(in_array('xx', $arrValues) !== false)
+		{
+			$arrReturn[$GLOBALS['TL_LANG']['CONTINENT']['other']]['xx'] = strlen($GLOBALS['TL_LANG']['CNT']['xx']) ? $GLOBALS['TL_LANG']['CNT']['xx'] : 'No Country';
+		}
 
-		// Add to the event.
-		return $return;
+		// Sort the keys.
+		ksort($arrReturn);
+
+		// Sort the sub-values.
+		foreach (array_keys($arrReturn) as $strConKey)
+		{
+			asort($arrReturn[$strConKey]);
+		}
+
+		return $arrReturn;
 	}
 }
