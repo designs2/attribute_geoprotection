e79e616799db132795557b09b53032e79da028ea
Mini Model <minimodel@metamodel.me>
2015-09-18 10:29:54 +0200

    Updated translations from transifex.

---
+++ contao/languages/ru/tl_metamodel_attribute.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-13T16:13:46+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']               = 'Параметры вывода';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['geoprotection']['0']           = 'Страны';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['geoprotection']['1']           = 'Выберите настраиваемые страны для редактора.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['geoprotection'] = 'Географическая защита (GeoProtection)';
+
